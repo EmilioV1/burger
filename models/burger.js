521977fe27ab4f9f5393aec5375f0c0e0eb305d0
Valladares <vallae@amazon.com>
2019-12-29 16:46:35 -0800

    Create added to burger.js.

--- models/burger.js
+++ models/burger.js
@@ -9,7 +9,11 @@ var burger = {
 
     update: function (id, cb) {
         orm.update("burgers", id, cb);
-    }
+    },
+
+    create: function(name, cb){
+        orm.create("burgers", name, cb);
+    },
 };
 
 module.exports = burger;
