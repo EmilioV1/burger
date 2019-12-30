3b1efa002e186e31a11b4d24bae93d3717725e82
Emilio Valladares <vallae.wumbo@gmail.com>
2019-12-29 19:54:35 -0800

    Fixed syntax error in orm for all function where db was referenced instead of cb.

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 var connection = require("./connection.js");
 
 var orm = {
-    all: function (tableInput, db) {
+    all: function (tableInput, cb) {
         connection.query("SELECT * FROM " + tableInput + ";", function (err, result) {
             if (err) throw err;
             cb(result);
