82c3c03443e4c79fd5ab06d1677da6d4c61f19c2
Emilio Valladares <vallae.wumbo@gmail.com>
2019-12-29 23:18:21 -0800

    Working on getting jawsDB connected and running.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id: " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3000,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
+
+connection.connect();
 
 module.exports = connection;
---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT into burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
+INSERT into burgers (burger_name, devoured) VALUES ("Hamburger", FALSE);
+INSERT into burgers (burger_name, devoured) VALUES ("Pizza burger", FALSE);
